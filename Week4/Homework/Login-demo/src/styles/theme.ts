5864d02b9d2e1ea116caf2a4cdaa341165e3c57e
hello-yujin <yuuuujiiiin@naver.com>
2025-11-19 10:13:46 +0900

    Feat: ThemeProvider 및 GlobalStyle 기본 설정 추가

---
+++ Week4/Homework/Login-demo/src/styles/theme.ts
@@ -0,0 +1,9 @@
+import type { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  colors: {
+    background: "#f8fafc",
+    primary: "#a8e5d5",
+    primaryDark: "#86c9b9",
+  },
+};
